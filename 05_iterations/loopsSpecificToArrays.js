83b13fbe235db8b905442694b46a0fcf7289496e
SAURABH <saurabh.d.0102@gmail.com>
2023-08-07 19:10:41 +0000

    lopps specific to arrays like for in, for of, for each

---
+++ 05_iterations/loopsSpecificToArrays.js
@@ -0,0 +1,20 @@
+//loops specific to arrays
+
+ // ["","",""] //strings in array
+ // [{},{},{}] // objects in array
+
+
+//  const arr=[1,2,3,4,5]
+//  for (const loopvar of arr) {
+//     console.log(loopvar);
+//  }
+
+//   const greeting="hello world!";
+//   for (const greet of greeting) {
+//     if(greet==" ")
+//     break;
+//     console.log(`each word is ${greet}`);
+//   }
+   
+//Maps
+  

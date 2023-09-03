7d2d862e2034a02d952fdc89a9829a714d8e48de
SAURABH <saurabh.d.0102@gmail.com>
2023-09-03 06:14:21 +0000

    ways of declaring strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+// console.log("hello world");
+const str="saurabh";
+const repocount=30;
+// console.log(name.+repocount.+"value");
+ console.log(`hi my name is ${str} and my repocount is ${repocount}`)
+ 

9f80b3449162276d961fff990c92396b51e27737
SAURABH <saurabh.d.0102@gmail.com>
2023-09-04 07:38:23 +0000

    Parent folder name changed

---
+++ 01_basics_Part_1/07_dateAndTime.js
@@ -0,0 +1,9 @@
+// console.log("date and time in js");
+
+const myDate=new Date();
+// console.log(myDate);
+console.log(typeof myDate);//object
+console.log(myDate.toString());//givs date and tiem and tiem zone and other detail
+console.log(myDate.toLocaleString());//gives date and time
+console.log(myDate.toLocaleDateString());//gives date in MM/DD/YYYY format
+console.log(myDate.toLocaleTimeString());//gives time only

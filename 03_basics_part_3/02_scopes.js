2d1d9aaefd23e9859b9e1152f891228443101c0d
SAURABH <saurabh.d.0102@gmail.com>
2023-09-09 20:51:42 +0000

    let,const,var difference

---
+++ 03_basics_part_3/02_scopes.js
@@ -0,0 +1,21 @@
+// console.log("welcome to scopes ");
+
+let a=10;
+const b=20;
+var c=30;
+
+if(true)
+{
+    let a=100
+    const b=200
+    c=300
+}
+console.log(a);//10
+console.log(b);//20
+console.log(c);//300 this is the issue with var
+
+
+
+         //********** lec 22 -Scope level and mini hoisting in javascript *************** */
+
+         
